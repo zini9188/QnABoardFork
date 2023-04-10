4c43af20b2b64373f5e6f9fa23334922c331263d
kys0411 <32180846@dankook.ac.kr>
2023-04-10 22:08:25 +0900

    add: BaseTime Entity 추가

--- src/main/java/krkr/board/domain/Member.java
+++ src/main/java/krkr/board/domain/Member.java
@@ -1,11 +1,16 @@
 package krkr.board.domain;
 
 import krkr.board.constant.Role;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
-public class Member {
+@Getter
+@NoArgsConstructor
+public class Member extends BaseTime{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,4 +23,10 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @Builder
+    public Member(String email, String password, Role role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
 }
